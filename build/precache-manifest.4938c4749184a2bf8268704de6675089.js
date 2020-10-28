b61f98f3ed5f68d2530f01f4bc6401640acb158a
AndreiD049 <andrei.dimitrascu94@gmail.com>
2020-10-28 23:27:23 +0200

    build

---
+++ build/precache-manifest.4938c4749184a2bf8268704de6675089.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aaf19accf4aef9a8dd32b470adb603c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd1215d44d705881302",
+    "url": "/static/js/2.7b1bd23f.chunk.js"
+  },
+  {
+    "revision": "5f983ef0d998e4957796cf6a040e4dc4",
+    "url": "/static/js/2.7b1bd23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2032cacfd917968dade0",
+    "url": "/static/js/3.d759f4c1.chunk.js"
+  },
+  {
+    "revision": "fd96f6cbe253bf0eb04f",
+    "url": "/static/js/4.f49235c8.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.f49235c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf01249b40181224c48",
+    "url": "/static/js/5.bcf17886.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.bcf17886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77873f20164abb8da55e",
+    "url": "/static/js/main.d415990a.chunk.js"
+  },
+  {
+    "revision": "4f23f19f09172ead9b18",
+    "url": "/static/js/runtime-main.5e905dae.js"
+  },
+  {
+    "revision": "29ecac2fdcbd8776cba96700c1453e4f",
+    "url": "/static/media/error.29ecac2f.svg"
+  },
+  {
+    "revision": "f49ce241442608b0eed64477d708374d",
+    "url": "/static/media/info.f49ce241.svg"
+  },
+  {
+    "revision": "ea116b8d7448242329aec28ce62823a2",
+    "url": "/static/media/success.ea116b8d.svg"
+  },
+  {
+    "revision": "6c796c9a3bfb812b6ede248a5c68c38d",
+    "url": "/static/media/warn.6c796c9a.svg"
+  }
+]);
